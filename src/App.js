52e04a6cf803b874fd5801412100d3e693b9abd2
Ahmed Mustafa <117983561+AhmedMustafa48@users.noreply.github.com>
2024-02-07 10:47:35 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  /// make a from have input fields of name email pass
+  // store them in one array of userInfo and all form data store in one state called info,setInfo .
+  // make only one one handle change method  which uses names of filed to target their value.
+  // after submit form field becomes empty
+
+  // const [name, setName] = useState("");
+  // const [pass, setPass] = useState("");
+  // const [email, setEmail] = useState("");
+
+  const initialState = {
+    name: "",
+    email: "",
+    password: "",
+  };
+  const [info, setInfo] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInfo((prevInfo) => ({
+      ...prevInfo,
+      [name]: value,
+    }));
+  };
+
+  // const nameChange = (e) => {
+  //   setName(e.target.value);
+  // };
+
+  // const passChange = (e) => {
+  //   setPass(e.target.value);
+  // };
+  // const emailChange = (e) => {
+  //   setEmail(e.target.value);
+  // };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ info });
+    setInfo(initialState);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="name"
+          type="text"
+          value={info.name}
+          placeholder="enter name"
+          onChange={handleChange}
+        />
+        <input
+          name="password"
+          type="password"
+          value={info.password}
+          placeholder="enter Password"
+          onChange={handleChange}
+        />
+        <input
+          name="email"
+          type="text"
+          value={info.email}
+          placeholder="enter Mail"
+          onChange={handleChange}
+        />
+
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
